7601bf242ab8890ec66cb7d97b0617e969150850
ElenaKhin <phyuthandarkhin22@gmail.com>
2024-10-10 20:53:45 +0700

    deploy

--- app/page.js
+++ app/page.js
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 // app/page.js
 import dbConnect from '../lib/mongodb';
 
@@ -7,20 +5,14 @@ export default async function Home({ searchParams }) {
   // Call the dbConnect function to establish the database connection
   await dbConnect();
 
-  // Retrieve the `name` parameter from the URL's search parameters
+  // Extract the name from searchParams
   const name = searchParams?.name || 'World';
 
   // Render the component with the greeting message
   return (
     <div>
-      <h1>Hello {name}</h1>
+      {/* Correctly escape single quotes */}
+      <h1>Hello, it&#39;s {name}!</h1>
     </div>
   );
 }
-
-
-// export default function Home({ params, searchParams }) {
-//   console.debug(params, searchParams);
-//   const name = searchParams['name'] || 'World';
-//   return <h1>Hello {name}</h1>;
-// }
